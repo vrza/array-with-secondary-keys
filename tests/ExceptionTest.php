05e5b88c6c58c5a7d7af6d5066766e2c19c78076
Vladimir Vrzić <vvrzic@gmail.com>
2021-05-17 14:05:10 +0200

    More exception tests

--- tests/ExceptionTest.php
+++ tests/ExceptionTest.php
@@ -6,7 +6,7 @@ use VladimirVrzic\ArrayWithSecondaryKeys\NoSuchIndexException;
 
 final class ExceptionTest extends TestCase
 {
-    public function testNoSuchIndexException(): void
+    public function testGetByIndexNoSuchIndexException(): void
     {
         $pid = 12345;
         $a = new ArrayWithSecondaryKeys();
@@ -22,5 +22,41 @@ final class ExceptionTest extends TestCase
         $a->createIndex('state.pid');
         $this->expectException(NoSuchIndexException::class);
         $a->getByIndex('state.foobar', $pid)['name'];
+    }
+
+    public function testSecondaryKeysNoSuchIndexException(): void
+    {
+        $pid = 12345;
+        $a = new ArrayWithSecondaryKeys();
+        $a->put(
+            22,
+            [
+                'name' => 'twenty-two',
+                'state' => [
+                    'pid' => $pid
+                ]
+            ]
+        );
+        $a->createIndex('state.pid');
+        $this->expectException(NoSuchIndexException::class);
+        $a->secondaryKeys('state.foobar');
+    }
+
+    public function testContainsSecondaryKeyNoSuchIndexException(): void
+    {
+        $pid = 12345;
+        $a = new ArrayWithSecondaryKeys();
+        $a->put(
+            22,
+            [
+                'name' => 'twenty-two',
+                'state' => [
+                    'pid' => $pid
+                ]
+            ]
+        );
+        $a->createIndex('state.pid');
+        $this->expectException(NoSuchIndexException::class);
+        $a->containsSecondaryKey('state.foobar', $pid);
     }
 }
