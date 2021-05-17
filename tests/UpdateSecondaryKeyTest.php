d78289bc8d84dff3c7948a740f0322003380a66c
Vladimir Vrzić <vvrzic@gmail.com>
2021-05-17 14:03:47 +0200

    Test updateByIndex

--- tests/UpdateSecondaryKeyTest.php
+++ tests/UpdateSecondaryKeyTest.php
@@ -38,4 +38,45 @@ final class UpdateSecondaryKeyTest extends TestCase
             $a->get('22.state.pid')
         );
     }
+
+    public function testUpdateByIndex(): void
+    {
+        $a = new ArrayWithSecondaryKeys();
+        $a->put(
+            22,
+            [
+                'name' => 'twenty-two',
+                'state' => [
+                    'pid' => 12345
+                ]
+            ]
+        );
+        $a->createIndex('state.pid');
+        $a->updateByIndex(
+            'state.pid',
+            12345,
+            [
+                'name' => 'twenty-three',
+                'state' => [
+                    'pid' => 12345
+                ]
+            ]
+
+        );
+        // update by non existing secondary key is a no-op
+        $a->updateByIndex(
+            'state.pid',
+            1,
+            [
+                'state' => [
+                    'pid' => 42
+                ]
+            ]
+        );
+        $this->assertFalse($a->containsSecondaryKey('state.pid', 42));
+        $this->assertEquals(
+            'twenty-three',
+            $a->get('22.name')
+        );
+    }
 }
