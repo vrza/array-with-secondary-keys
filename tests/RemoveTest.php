8ba8dbd77e2f53eca2bcd7cb78974e4cfcb1dc5d
Vladimir Vrzić <vvrzic@gmail.com>
2021-05-17 13:19:42 +0200

    Cover remove and removeByIndex methods

--- tests/RemoveTest.php
+++ tests/RemoveTest.php
@@ -6,6 +6,49 @@ use VladimirVrzic\ArrayWithSecondaryKeys\NoSuchIndexException;
 
 final class RemoveTest extends TestCase
 {
+    public function testRemove(): void
+    {
+        $a = new ArrayWithSecondaryKeys();
+        $a->put(
+            22,
+            [
+                'name' => 'twenty-two',
+                'state' => [
+                    'pid' => 12345,
+                    'db' => 'UPDATED'
+                ]
+            ]
+        );
+        $a->createIndex('state.pid');
+        $a->remove('22.state.pid');
+        $this->assertFalse(
+            $a->has('22.state.pid')
+        );
+        $this->assertEquals(
+            'UPDATED',
+            $a->get('22.state.db')
+        );
+    }
+
+    public function testRemoveByIndex(): void
+    {
+        $pid = 12345;
+        $a = new ArrayWithSecondaryKeys();
+        $a->put(
+            22,
+            [
+                'name' => 'twenty-two',
+                'state' => [
+                    'pid' => $pid,
+                    'db' => 'UPDATED'
+                ]
+            ]
+        );
+        $a->createIndex('state.pid');
+        $a->removeByIndex('state.pid', $pid);
+        $this->assertTrue($a->isEmpty());
+    }
+
     public function testRemoveIndexedValue(): void
     {
         $pid = 12345;
