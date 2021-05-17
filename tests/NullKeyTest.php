df7ad1ceea8118c0bafcb97dbdd0f79f195dfb6f
Vladimir Vrzić <vvrzic@gmail.com>
2021-05-17 15:01:35 +0200

    Test has and get with null key

---
+++ tests/NullKeyTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use VladimirVrzic\ArrayWithSecondaryKeys\ArrayWithSecondaryKeys;
+
+final class NullKeyTest extends TestCase
+{
+    public function testNullKey(): void
+    {
+        $a = new ArrayWithSecondaryKeys();
+        $a->put(
+            22,
+            [
+                'name' => 'twenty-two',
+                'state' => [
+                    'pid' => 12345
+                ]
+            ]
+        );
+        $this->assertFalse(
+            $a->has(null)
+        );
+        $this->assertEquals(
+            $a->get(null),
+            $a->asArray()
+        );
+    }
+}
